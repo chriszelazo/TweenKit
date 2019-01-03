800923b5576392b7762120bcc7407d90099313bf
Chris Zelazo <christopherzelazo@pinterest.com>
2019-01-03 14:20:48 -0800

    Added control point strings for easing curves

--- TweenKit/TweenKit/EasingCurve.swift
+++ TweenKit/TweenKit/EasingCurve.swift
@@ -22,6 +22,77 @@ public struct Easing {
     public func apply(_ t: Double) -> Double {
         return curve.function(with: mode)(t)
     }
+    
+    public var controlPoints: (point1: String, point2: String) {
+        switch curve {
+        case .linear:
+            switch mode {
+            case .easeIn:    fallthrough
+            case .easeOut:   fallthrough
+            case .easeInOut: return ("(0, 0)", "(1, 1)")
+            }
+        case .sine:
+            switch mode {
+            case .easeIn:    return ("(0.47, 0)", "(0.745, 0.715)")
+            case .easeOut:   return ("(0.39, 0.575)", "(0.565, 1)")
+            case .easeInOut: return ("(0.445, 0.05)", "(0.55, 0.95)")
+            }
+        case .quadratic:
+            switch mode {
+            case .easeIn:    return ("(0.55, 0.085)", "(0.68, 0.53)")
+            case .easeOut:   return ("(0.25, 0.46)", "(0.45, 0.94)")
+            case .easeInOut: return ("(0.455, 0.03)", "(0.515, 0.955)")
+            }
+        case .cubic:
+            switch mode {
+            case .easeIn:    return ("(0.55, 0.055)", "(0.675, 0.19)")
+            case .easeOut:   return ("(0.215, 0.61)", "(0.355, 1)")
+            case .easeInOut: return ("(0.645, 0.045)", "(0.355, 1)")
+            }
+        case .quartic:
+            switch mode {
+            case .easeIn:    return ("(0.895, 0.03)", "(0.685, 0.22)")
+            case .easeOut:   return ("(0.165, 0.84)", "(0.44, 1)")
+            case .easeInOut: return ("(0.77, 0)", "(0.175, 1)")
+            }
+        case .quintic:
+            switch mode {
+            case .easeIn:    return ("(0.755, 0.05)", "(0.855, 0.06)")
+            case .easeOut:   return ("(0.23, 1)", "(0.32, 1)")
+            case .easeInOut: return ("(0.86, 0)", "(0.07, 1)")
+            }
+        case .exponential:
+            switch mode {
+            case .easeIn:    return ("(0.95, 0.05)", "(0.795, 0.035)")
+            case .easeOut:   return ("(0.19, 1)", "(0.22, 1)")
+            case .easeInOut: return ("(1, 0)", "(0, 1)")
+            }
+        case .circular:
+            switch mode {
+            case .easeIn:    return ("(0.6, 0.04)", "(0.98, 0.335)")
+            case .easeOut:   return ("(0.075, 0.82)", "(0.165, 1)")
+            case .easeInOut: return ("(0.785, 0.135)", "(0.15, 0.86)")
+            }
+        case .back:
+            switch mode {
+            case .easeIn:    return ("(0.6, -0.28)", "(0.735, 0.045)")
+            case .easeOut:   return ("(0.175, 0.885)", "(0.32, 1.275)")
+            case .easeInOut: return ("(0.68, -0.55)", "(0.265, 1.55)")
+            }
+        case .elastic:
+            switch mode {
+            case .easeIn:    return ("-", "-")
+            case .easeOut:   return ("-", "-")
+            case .easeInOut: return ("-", "-")
+            }
+        case .bounce:
+            switch mode {
+            case .easeIn:    return ("-", "-")
+            case .easeOut:   return ("-", "-")
+            case .easeInOut: return ("-", "-")
+            }
+        }
+    }
 }
 
 public enum EasingMode: String, CaseIterable {
